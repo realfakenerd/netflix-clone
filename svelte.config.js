2093016a8118cc63d07edf34acef62c6f54ecb26
realfakenerd <lucas210898@hotmail.com>
2021-10-06 00:40:31 -0300

    initial commit

---
+++ svelte.config.js
@@ -0,0 +1,11 @@
+/** @type {import('@sveltejs/kit').Config} */
+import firebase from 'svelte-adapter-firebase'
+const config = {
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+		adapter: firebase()
+	}
+};
+
+export default config;
